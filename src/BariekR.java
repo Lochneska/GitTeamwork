a06e614bca549b84b3a33e8f98acd3440e04f1bc
Marek Lochman <lochman59@gmail.com>
2023-12-04 12:25:38 +0100

    a3

--- src/BariekR.java
+++ src/BariekR.java
@@ -1,5 +1,5 @@
 public class BariekR {
     public void count() {
-        System.out.println("My age is 28 " + 1);
+        System.out.println("My ageeeeeeeeeeeeeeeeeeeeeeeeeeee is 28 " + 1);
     }
 }
