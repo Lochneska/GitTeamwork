80f90ed956a1d024ef6e40df978ea95e1bfc35a1
Marek Lochman <lochman59@gmail.com>
2023-12-04 13:53:12 +0100

    a6

--- src/BilekP.java
+++ src/BilekP.java
@@ -1,7 +1,6 @@
 public class BilekP {
 
-    public void cheer(int numberParam) {
-        System.out.println("PavelSlimBilek in Java..." + numberParam);
-        System.out.println("Fuckin modifikace");
+    public void cheer(String parameter) {
+        System.out.println("Cheer for the " + parameter);
     }
 }
