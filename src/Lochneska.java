2e24d71106c93a4d806fe341767818b0bb9b3490
Marek Lochman <lochman59@gmail.com>
2023-12-04 11:59:05 +0100

    a1

---
+++ src/Lochneska.java
@@ -0,0 +1,11 @@
+public class Lochneska {
+
+    public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet(){
+        System.out.println("Lochneska");
+    }
+
+}
